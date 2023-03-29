5c94e25b7bfcf60a490273cffd96cf0cc76fbcaf
kagaba-etienne <kagabaetienne04@gmail.com>
2023-03-29 20:26:55 +0200

    ch-database-setup-config: setting up database configuration

---
+++ database/config/config.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const development = {
+  url: process.env.DEV_DATABASE_URL,
+  dialect: 'postgres',
+}
+export const test = {
+  url: process.env.TEST_DATABASE_URL,
+  dialect: 'postgres',
+}
+export const production = {
+  url: process.env.TEST_DATABASE_URL,
+  dialect: 'postgres',
+}
