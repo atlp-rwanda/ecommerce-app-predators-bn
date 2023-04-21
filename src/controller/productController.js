2a2e4b43a584a08de993c86267bedd2dac3bcd9d
Faid Terence JABO <faidterence@outlook.com>
2023-04-21 18:23:08 +0200

    ft(product):Delete Product from seller collection functionalities

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -92,4 +92,43 @@ export const updateProduct = async (req, res) => {
   });
 };
 
-export default { getAllProducts, getProductById, updateProduct };
+export const deleteSpecificProduct = async (req, res) =>{
+  try {
+      // Validate input data
+      const {reason} = req.body;
+      const productId = parseInt(req.params.id);
+      if (isNaN(productId) || typeof reason !== "string" || reason.trim() === "") {
+          return res.status(400).json({
+              status: "fail",
+              data: { message: "Invalid input data" }
+          });
+      }
+
+      const isAvailable = await db.Product.findOne({ where: { id: itemId, vendor_id: req.user.id } });
+      if(!isAvailable){
+          // If the product is not found in the collection, return a JSend fail response with an appropriate message
+          return res.status(401).json({
+              status: "fail",
+              data: { message: "Can not find such product in your collection" }
+          });
+      }
+      else {
+          // If the product is found, delete it and return a JSend success response with a message indicating the reason for deletion
+          await isAvailable.destroy();
+          return res.status(200).json({
+              status: "success",
+              data: { message: `This product has been removed because of the following reason: ${reason}.` }
+          });
+      }
+  } catch (error) {
+      // If there's any error, return a JSend error response with an appropriate message
+      console.error(error);
+      return res.status(500).json({
+          status: "error",
+          data: { message: "Oops, something went wrong on the server side. Please try again later."}
+      });
+  }
+}
+
+
+export default { getAllProducts, getProductById, updateProduct, deleteSpecificProduct };
--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,4 +1,4 @@
-import {getAllProducts, getProductById, updateProduct} from '../controller/productController';
+import {deleteSpecificProduct, getAllProducts, getProductById, updateProduct} from '../controller/productController';
 import { isAdmin, isSeller, isBuyer, checkPermission } from "../middleware/roles.js";
 import { Router } from "express";
 
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/product', getAllProducts);
 router.get('/product/:id', getProductById);
 router.put('/product/:id',isSeller, updateProduct);
+router.delete('/product/:id',isSeller, deleteSpecificProduct);
 
 export default router;
